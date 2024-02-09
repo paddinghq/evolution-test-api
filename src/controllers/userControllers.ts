2ea0728453ba965a913303db442cb8188f83cc54
sudodeo <adesolaadeoluwa@gmail.com>
2024-02-09 08:54:32 +0100

    add get user feature

--- src/controllers/userControllers.ts
+++ src/controllers/userControllers.ts
@@ -1,22 +1,20 @@
-
-import { Request, Response } from 'express';
-import userService from '../services/userServices';
-
+import { type NextFunction, type Request, type Response } from "express";
+import userService from "../services/userServices";
 
 class UserController {
-    private userService: typeof userService;
+  private readonly userService: typeof userService;
   constructor() {
     this.userService = userService;
   }
-  /**
-   * @route GET api/v1/users.
-   * @desc get all registered users
-   * @access Public.
-   */
-   getUsers = async (req:Request, res:Response) => {
-    await this.userService.getAllUsers(req, res);
-  };
 
+  /**
+   * @route GET api/v1/users/:id
+   * @desc get a registered user
+   * @access Private
+   */
+  getUser = async (req: Request, res: Response, next: NextFunction) => {
+    await this.userService.getUser(req, res, next);
+  };
 }
 
 export default UserController;
---
+++ src/middlewares/authorizeUser.ts
@@ -0,0 +1,69 @@
+import { UserModel } from "../models/userModel";
+import { verifyToken } from "../utils/authorization";
+import { NextFunction, Request, Response } from "express";
+import { IUser } from "../types/types";
+import { Forbidden, Unauthorized } from "./errorHandler";
+
+// Extend the Request interface to include a custom property
+declare global {
+  namespace Express {
+    interface Request {
+      authUser?: IUser;
+    }
+  }
+}
+
+export const authorizeUser = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) => {
+  try {
+    // Retrieve the token from the Authorization header in the request
+    const authorizationHeader = req.headers.authorization;
+    
+
+    if (!authorizationHeader) {
+      throw new Unauthorized("Token not found");
+    }
+
+    // Extract the token without the "Bearer" prefix
+    const token = authorizationHeader.split(" ")[1];
+    
+
+    if (!token) {
+      throw new Unauthorized("Invalid token");
+    }
+
+    const decodedUser: any | undefined = verifyToken(token);
+
+    if (decodedUser == null) {
+      throw new Forbidden("Invalid token");
+    }
+
+    const user: IUser | undefined | null = await UserModel.findById(
+      decodedUser.id
+    );
+
+    if (user == null) {
+      throw new Unauthorized("User not found");
+    }
+
+    const lastTokenDate = decodedUser.iat * 1000;
+    const passwordUpdatedTime = user.lastChangedPassword?.getTime() || 0;
+
+    // Further user authentication
+    if (lastTokenDate < passwordUpdatedTime) {
+      throw new Unauthorized(
+        "Your cookie session has expired, please login again"
+      );
+    }
+
+    // Store authenticated user id in the request object.
+    req.authUser = user;
+
+    return next();
+  } catch (error) {
+    next(error);
+  }
+};
