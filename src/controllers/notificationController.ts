25a4af13a1190e4f03ddb34056484c4cf6066c6e
sudodeo <adesolaadeoluwa@gmail.com>
2024-02-16 21:36:32 +0100

    add unread notifications count feat
    also added feature to mark all notifications as read

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -1,31 +1,29 @@
 import { Request, Response, NextFunction } from "express";
 import NotificationService from "../services/notificationService";
 import INotification from "../types/notificationType";
-
-
+import { Unauthorized } from "../middlewares/errorHandler";
 
 export const getNotifications = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ) => {
-    try {
-  
-      const notifications = await NotificationService.getNotifications();
-  
-      res.status(200).json({
-        message:
-          notifications.length > 0
-            ? "Notification retrieved successfully"
-            : "You have no notifications",
-        body: notifications,
-        success: true,
-        statusCode: 200,
-      });
-    } catch (error: any) {
-        next(error);
-    }
-  };
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const notifications = await NotificationService.getNotifications();
+
+    res.status(200).json({
+      message:
+        notifications.length > 0
+          ? "Notification retrieved successfully"
+          : "You have no notifications",
+      body: notifications,
+      success: true,
+      statusCode: 200,
+    });
+  } catch (error: any) {
+    next(error);
+  }};
+
 
   // get notification by id
   export const getNotification = async (
@@ -80,3 +78,58 @@ export const getNotifications = async (
   };
 
   
+
+export const countUnreadNotifications = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.authUser?.id;
+
+    if (userId == null) {
+      throw new Unauthorized(
+        "You do not have permissions to perform this action"
+      );
+    }
+
+    const unreadNotificationCount =
+      await NotificationService.countUnreadNotifications({
+        userId,
+        read: false,
+      });
+
+    res.status(200).json({
+      message: "Unread notifications count retrieved successfully",
+      count: unreadNotificationCount,
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const markAllAsRead = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const userId = req.authUser?.id;
+
+    if (userId == null) {
+      throw new Unauthorized(
+        "You do not have permissions to perform this action"
+      );
+    }
+
+    await NotificationService.markAllAsRead(userId);
+
+    res.status(200).json({
+      message: "All notifications marked as read",
+      success: true,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/notification.ts
+++ src/routes/notification.ts
@@ -1,12 +1,19 @@
 import express from "express";
+<<<<<<< HEAD
 import { getNotifications, getNotification, removeNotification } from "../controllers/notificationController";
+=======
+import {
+  countUnreadNotifications,
+  getNotifications,
+  markAllAsRead,
+} from "../controllers/notificationController";
+>>>>>>> f20f859 (add unread notifications count feat)
 
 const router = express.Router();
 
-
-
 router.get("/", getNotifications);
 
+<<<<<<< HEAD
 // Get Notification by id
 router.get("/:id", getNotification)
 
@@ -15,3 +22,10 @@ router.delete("/:id", removeNotification)
 
 
 export default router;
+=======
+router.get("/unread-count", countUnreadNotifications);
+
+router.put("/mark-all-as-read", markAllAsRead);
+
+export default router;
+>>>>>>> f20f859 (add unread notifications count feat)
