39c6f23f3550ef48256d45d3d682b565e4941f53
PATIENCE-ONAH <ifypatience10.pi@gmail.com>
2024-02-18 09:52:57 +0100

    Implemented get and remove notification

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import NotificationService from "../services/notificationService";
+import INotification from "../types/notificationType";
 
 
 
@@ -25,3 +26,57 @@ export const getNotifications = async (
         next(error);
     }
   };
+
+  // get notification by id
+  export const getNotification = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+
+      const notificationId = req.params.id
+
+      const notification = await NotificationService.getNotification(notificationId);
+  
+      res.status(200).json({
+        message:
+          notification
+            ? "Notification retrieved successfully"
+            : "You have no notifications",
+        body: notification,
+        success: true,
+        statusCode: 200,
+      });
+    } catch (error: any) {
+        next(error);
+    }
+  };
+
+  // Remove notification
+  export const removeNotification = async (
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    try {
+
+      const notificationId = req.params.id
+
+      const removedNotification = await NotificationService.removeNotification(notificationId);
+  
+      res.status(200).json({
+        message:
+          removedNotification 
+            ? "Notification removed successfully"
+            : "You have no notifications",
+        body: removedNotification,
+        success: true,
+        statusCode: 200,
+      });
+    } catch (error: any) {
+        next(error);
+    }
+  };
+
+  
